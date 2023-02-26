3fe4b87861b92c78c4c3ae435b631c6b23ad9bf1
Shaskels <zalyaeva-03@mail.ru>
2023-02-26 21:06:22 +0700

    CommandParse

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
-
+import Input.Program;
+import Commands.Handler;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Program cp = new Program();
+        cp.Parse(args);
+        Handler h = new Handler(cp);
+
     }
 }
